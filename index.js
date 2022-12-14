db702536dca6a05f55c9814cf9731d79abb278fd
Flynn (Mr. Robot) <flynnwebdev@gmail.com>
2022-12-14 14:01:57 +1000

    Added GET entries/:id route

--- index.js
+++ index.js
@@ -19,14 +19,23 @@ app.get('/categories', (req, res) => res.send(categories))
 
 app.get('/entries', (req, res) => res.send(entries))
 
+app.get('/entries/:id', (req, res) => {
+  const entry = entries[req.params.id]
+  if (entry) {
+    res.send(entry)
+  } else {
+    res.status(404).send({ error: 'Entry not found' })
+  }
+})
+
 app.post('/entries', (req, res) => {
-  // 1. Create a new entry object with values passed in from the reques
+  // 1. Create a new entry object with values passed in from the request
   const { category, content } = req.body
-  // Validation/sanitize
   const newEntry = { category, content }
-  res.send(req.body)
   // 2. Push the new entry to the entries array
+  entries.push(newEntry)
   // 3. Send the new entry with 201 status
+  res.status(201).send(newEntry)
 })
 
 app.listen(port, () => console.log(`App running at http://localhost:${port}/`))
